2cc136cd621e4917286aa3c51f094cab4f35bbf7
Prince Kumar <kumarprince1407@gmail.com>
2023-07-10 13:07:40 +0530

    Added Use Case 1

--- EmployeeManagement3/src/com/bridgelabz/employeemanagement3/EmployeeManagement3.java
+++ EmployeeManagement3/src/com/bridgelabz/employeemanagement3/EmployeeManagement3.java
@@ -1,7 +1,15 @@
 package com.bridgelabz.employeemanagement3;
-
+import java.util.*;
 public class EmployeeManagement3 {
+    //UC1
     public static void main(String[] args) {
         System.out.println("Welcome to employee management using classes, version - 3.");
+        Random random=new Random();
+        int attendance = random.nextInt(2);
+        if(attendance==1){
+        System.out.println("The employee is present.");
+    }else{
+            System.out.println("The employee is absent.");
+        }
     }
 }
