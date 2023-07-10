51788e2985c03639e2101f3c50e5633989db5087
Prince Kumar <kumarprince1407@gmail.com>
2023-07-10 12:58:08 +0530

    Added initial commit

---
+++ EmployeeManagement3/src/com/bridgelabz/employeemanagement3/EmployeeManagement3.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.employeemanagement3;
+
+public class EmployeeManagement3 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to employee management using classes, version - 3.");
+    }
+}
