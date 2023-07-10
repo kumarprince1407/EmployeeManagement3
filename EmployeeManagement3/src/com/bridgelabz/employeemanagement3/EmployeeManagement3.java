ab5e99ac0df31a79f77db13392b83e01c474f17e
Prince Kumar <kumarprince1407@gmail.com>
2023-07-10 13:20:28 +0530

    Added Use Case 3

--- EmployeeManagement3/src/com/bridgelabz/employeemanagement3/EmployeeManagement3.java
+++ EmployeeManagement3/src/com/bridgelabz/employeemanagement3/EmployeeManagement3.java
@@ -1,22 +1,28 @@
 package com.bridgelabz.employeemanagement3;
 import java.util.*;
 public class EmployeeManagement3 {
-    //UC2
-    /*Calculate Daily
-Employee Wage - Assume Wage per Hour is 20 - Assume Full Day Hour is 8*/
+    //UC3
+    /*Add Part time
+Employee & Wage
+- Assume Part time Hour is 8*/
     static final int wagePerHour=20;
-    static final int fulldayHours=8;
+    static final int fullDayHours=8;
+    static final int partTimeHours=4;
     static int dailyWage=0;
     public static void main(String[] args) {
         System.out.println("Welcome to employee management using classes, version - 3.");
         Random random=new Random();
-        int attendance = random.nextInt(2);
-        dailyWage=wagePerHour*fulldayHours;
+        int attendance = random.nextInt(3);
+        dailyWage=wagePerHour*fullDayHours;
         if(attendance==1){
-        System.out.println("The employee is present.");
-            dailyWage=wagePerHour*fulldayHours;
-    }else{
-            System.out.println("The employee is absent.");
+        System.out.println("The employee is present full-time.");
+            dailyWage=wagePerHour*fullDayHours;
+    }else if(attendance==2){
+            System.out.println("The employee is present part-time.");
+            dailyWage=wagePerHour*partTimeHours;
+        }
+        else{
+            System.out.println("Employee is absent.");
             dailyWage=0;
         }
         System.out.println("Daily wage: "+ dailyWage);
