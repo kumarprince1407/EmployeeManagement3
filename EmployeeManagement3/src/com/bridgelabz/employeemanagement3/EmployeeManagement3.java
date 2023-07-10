bbd0d70fdb4b217fdc315c27c2f63538a31e8654
Prince Kumar <kumarprince1407@gmail.com>
2023-07-10 13:29:46 +0530

    Added Use Case 4

--- EmployeeManagement3/src/com/bridgelabz/employeemanagement3/EmployeeManagement3.java
+++ EmployeeManagement3/src/com/bridgelabz/employeemanagement3/EmployeeManagement3.java
@@ -1,10 +1,10 @@
 package com.bridgelabz.employeemanagement3;
 import java.util.*;
 public class EmployeeManagement3 {
-    //UC3
-    /*Add Part time
-Employee & Wage
-- Assume Part time Hour is 8*/
+    //UC4
+    /*Solving using
+Switch Case
+Statement*/
     static final int wagePerHour=20;
     static final int fullDayHours=8;
     static final int partTimeHours=4;
@@ -13,18 +13,22 @@ Employee & Wage
         System.out.println("Welcome to employee management using classes, version - 3.");
         Random random=new Random();
         int attendance = random.nextInt(3);
-        dailyWage=wagePerHour*fullDayHours;
-        if(attendance==1){
-        System.out.println("The employee is present full-time.");
-            dailyWage=wagePerHour*fullDayHours;
-    }else if(attendance==2){
-            System.out.println("The employee is present part-time.");
-            dailyWage=wagePerHour*partTimeHours;
+
+        switch (attendance){
+            case 0:
+                System.out.println("Employee is absent.");
+                dailyWage=0;
+                break;
+            case 1:
+                System.out.println("Employee is present full-time.");
+                dailyWage=fullDayHours*wagePerHour;
+                break;
+            case 2:
+                System.out.println("Employee is present part-time.");
+                dailyWage=partTimeHours*wagePerHour;
+                break;
         }
-        else{
-            System.out.println("Employee is absent.");
-            dailyWage=0;
-        }
+
         System.out.println("Daily wage: "+ dailyWage);
 
     }
