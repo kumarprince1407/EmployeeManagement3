0a7ae4a6ad8456d0d4a96018c2d5623a8fd4d5d1
Prince Kumar <kumarprince1407@gmail.com>
2023-07-12 09:29:31 +0530

    Updated Use case 5

--- EmployeeManagement3/src/com/bridgelabz/employeemanagement3/EmployeeManagement3.java
+++ EmployeeManagement3/src/com/bridgelabz/employeemanagement3/EmployeeManagement3.java
@@ -2,9 +2,11 @@ package com.bridgelabz.employeemanagement3;
 import java.util.*;
 public class EmployeeManagement3 {
     //UC5
-    /*Calculating Wages
-for a Month
-- Assume 20 Working Day per Month*/
+    /*Calculate Wages till
+a condition of total
+working hours or
+days is reached for
+a month - Assume 100 hours and 20 days*/
     static final int wagePerHour=20;
     static final int fullDayHours=8;
     static final int partTimeHours=4;
@@ -12,32 +14,40 @@ for a Month
     static int totalWorkingDays=0;
     static int monthlyWage=0;
     static int dailyWage=0;
+    static int totalWorkingHours=0;
+    static final int maxWorkingHours=100;
+    static int workingHours=0;
     public static void main(String[] args) {
         System.out.println("Welcome to employee management using classes, version - 3.");
         Random random = new Random();
 
-        while (totalWorkingDays<maxWorkingDays) {
+        while (totalWorkingDays<maxWorkingDays || totalWorkingHours<maxWorkingHours) {
             int attendance = random.nextInt(3);
             switch (attendance) {
                 case 0:
                     System.out.println("Employee is absent.");
                     dailyWage = 0;
+                    workingHours=0;
                     break;
                 case 1:
                     System.out.println("Employee is present full-time.");
-                    dailyWage = fullDayHours * wagePerHour;
+                    workingHours=fullDayHours;
+                    dailyWage = workingHours * wagePerHour;
                     break;
                 case 2:
                     System.out.println(" Employee is present part-time.");
-                    dailyWage = partTimeHours * wagePerHour;
+                    workingHours=partTimeHours;
+                    dailyWage = workingHours * wagePerHour;
                     System.out.println("Daily wage: " + dailyWage);
                     break;
             }
             System.out.println("Daily wage: " + dailyWage);
             monthlyWage+=dailyWage;
+            totalWorkingHours+=workingHours;
             totalWorkingDays++;
 
         }
+        System.out.println("Total working hours: "+ totalWorkingHours);
         System.out.println("Monthly salary: "+ monthlyWage);
 
 
